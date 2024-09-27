999c0debe88de7321c74d62bba61fa47e02c1320
Unais <unaissk1234@gmail.com>
2024-09-27 18:11:59 +0530

    project push to github

---
+++ components/index.js
@@ -0,0 +1,19 @@
+import CustomButton from "./CustomButton";
+import FormField from "./FormField";
+// import VideoCard from "./VideoCard";
+// import InfoBox from "./InfoBox";
+// import Loader from "./Loader";
+// import Trending from "./Trending";
+// import SearchInput from "./SearchInput";
+// import EmptyState from "./EmptyState";
+
+export {
+    CustomButton,
+    FormField,
+//   VideoCard,
+//   InfoBox,
+//   Loader,
+//   Trending,
+//   SearchInput,
+//   EmptyState,
+};
