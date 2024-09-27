04e3acdd6a754b9f7e5e1289f09c1ec9cb7f51a2
Unais <unaissk1234@gmail.com>
2024-09-27 20:28:05 +0530

    hide sensitive Info to .env

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: ["nativewind/babel"],
+//   };
+// };
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["nativewind/babel"],
+    plugins: [
+      "nativewind/babel", // NativeWind for Tailwind CSS in React Native
+      [
+        "module:react-native-dotenv", // Plugin to load environment variables
+        {
+          moduleName: "@env", // Allows you to import env variables
+          path: ".env", // Path to your .env file
+        },
+      ],
+    ],
   };
 };
